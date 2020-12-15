6f8326bc5d14412c69bfc105077cd804a38b2b16
brtvnplt <bart.vanpelt@gmail.com>
2020-12-15 16:40:44 +0100

    First release

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '25c3b6d9263149a7bed4230a675b5856'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e033e2da-b17a-41f6-b871-97723d326546'
+};
